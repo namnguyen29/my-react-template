def5bdd90a673df04761ea2b94a38b1d527f7444
nnam <quocnam.dn29@gmail.com>
2024-12-30 11:26:42 +0700

    create horizontal media scroller

--- src/app/features/common/components/MouseTracker/MouseTracker.tsx
+++ src/app/features/common/components/MouseTracker/MouseTracker.tsx
@@ -1,11 +1,13 @@
 import { MouseEvent, ReactNode, useState } from 'react';
 
 type Props = {
+  zone: ReactNode;
   children: (value: { x: number; y: number }) => ReactNode;
 };
 
-export const MouseTracker = ({ children }: Props) => {
+export const MouseTracker = ({ children, zone }: Props) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+
   const handleMouseMove = (event: MouseEvent<HTMLDivElement>): void => {
     setPosition((prev) => ({
       ...prev,
@@ -15,19 +17,15 @@ export const MouseTracker = ({ children }: Props) => {
   };
 
   return (
-    <div
-      onMouseMove={handleMouseMove}
-      style={{
-        border: '2px solid red',
-        padding: '1rem'
-      }}
-    >
-      <p style={{ color: 'red' }}>Track my position in this div</p>
+    <div role="tooltip" className="border-2 border-red-500 p-4" onMouseMove={handleMouseMove}>
+      <p className="text-violet-300">Track my position in this div</p>
       <ul>
         <li>x: {position.x}</li>
         <li>y: {position.y}</li>
+        <li>{children({ x: position.x, y: position.y })}</li>
       </ul>
-      {children({ x: position.x, y: position.y })}
+
+      <div className="bg-violet-200">{zone}</div>
     </div>
   );
 };
--- src/app/features/common/components/index.ts
+++ src/app/features/common/components/index.ts
@@ -5,3 +5,4 @@ export * from './TemperatureInput/TemperatureInput';
 export * from './WatchTimer/WatchTimer';
 export * from './MouseTracker/MouseTracker';
 export * from './MouseTracker/ChildTracker/ChildTracker';
+export * from './ColorfulSection';
